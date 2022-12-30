d99ed35cb911c96acecb7933b82ea0412b057d1a
Morgan Schumann <91375960+morg-an@users.noreply.github.com>
2022-12-30 12:46:06 -0600

    Update day7a.py
    add getFolderString() and use currentFolder as a list, rather than string to aid in traversing the tree structure

--- day7a.py
+++ day7a.py
@@ -1,10 +1,9 @@
 def readfile():
-    with open("day7sample.txt", 'r') as terminalOutput:
+    with open("day7input.txt", 'r') as terminalOutput:
         terminalOutput = terminalOutput.readlines()
     return terminalOutput
 
 class Folder():
-    numFolders = 0
 
     def __init__(self, name, path, parent=None):
         self.name = name
@@ -12,7 +11,7 @@ class Folder():
         self.parent = parent
         self.files = []
         self.children = []
-        print(f"Folder Added: {self.name} in {self.path}")
+        print(f"Directory Added: {self.name}")
     
     def get_name(self):
         return self.name
@@ -34,12 +33,11 @@ class Folder():
     
     def add_child(self, folder):
         self.children.append(folder)
-        Folder.numFolders += 1
 
     def print_info(self):
-        print("Folder Children", len(self.get_children()), 
-        "Folder Docs: ", len(self.get_files()), 
-        "Folder Size:", self.get_folder_size())
+        print("Directories: ", len(self.get_children()), 
+        "    Files: ", len(self.get_files()), 
+        "    Total Size:", self.get_folder_size())
 
     def get_folder_size(self):
         value = 0
@@ -50,9 +48,9 @@ class Folder():
 class Document():
     def __init__(self, name, path, size):
         self.name = name
-        self.size = int(size)
         self.path = path
-        print(f"Document Added: {self.name} ({str(self.size)}) in {self.path}")
+        self.size = int(size)
+        print(f"File Added: {self.name} ({str(self.size)})    Path: {self.path}")
 
     def get_size(self):
         return self.size
@@ -76,39 +74,55 @@ def parceTerminalOutput(terminalOutput):
         parcedOutput.append(lineFunction)
     return parcedOutput
 
+def getFolderString(currentFolderList):
+    folderString = "/"
+    nonRootFolders = currentFolderList[1:]
+    for folder in nonRootFolders:
+        folderString += folder
+        folderString += '/'
+    if len(folderString) > 1:
+        folderString = folderString[:-1]
+    return folderString
+
 def buildFileStructure(parcedOutput):
-    i = 0
+    currentFolder = ['/']
     folderString = '/'
-    root = Folder('/', folderString)
-    Folder.numFolders += 1
+    root = Folder(folderString, currentFolder[0])
     folder = root
+
+    i = 0
     while i < len(parcedOutput):
         line = parcedOutput[i]
         if line[0] == "Change Directory":
             if line[1] == '/':
-                folderString = '/'
+                j = 1
+                while j < len(currentFolder):
+                    currentFolder.pop(-1)
+                    print(currentFolder)
+                    j += 1
                 folder = root
             elif line[1] == '..':
-                folderString = folderString[:-1]
+                currentFolder.pop(-1)
                 if folder != root:
                     folder = folder.get_parent()
             else:
-                folderString = folderString + line[1]
-                fileName = folderString
+                currentFolder.append(line[1])
+                fileName = currentFolder[-1]
                 folderChildren = folder.get_children()
                 for child in folderChildren:
                     if line[1] == child.get_name():
                         folder = child
-            print(f"Directory Changed To: {folderString} ({folder.get_name()})")
+            folderString = getFolderString(currentFolder)
+            print(f"Directory Changed To: {folder.get_name()}    path: {folder.get_path()}")
 
         elif line[0] == "Contains Directory":
             fileName = folderString
-            fileName = Folder(line[1], folderString, folder)
+            fileName = Folder(line[1], currentFolder, folder)
             folder.add_child(fileName)
             folder.print_info()
             
         elif line[0] == "Contains File":
-            document = Document(line[1][1], folderString, line[1][0])
+            document = Document(line[1][1], currentFolder, line[1][0])
             folder.add_file(document)
             folder.print_info()
 
