639890ca78afade1f9bdcf182a10416971448661
Morgan Schumann <91375960+morg-an@users.noreply.github.com>
2023-01-06 15:27:35 -0600

    create Node class and generate all Node instances

--- day 12/day12a.py
+++ day 12/day12a.py
@@ -1,9 +1,41 @@
 def readFile():
-    with open("day12sample.txt", "r") as rawHeightMap:
+    with open('day12sample.txt', "r") as rawHeightMap:
         heightMap = rawHeightMap.readlines()
         for i in range(len(heightMap)):
             heightMap[i] = heightMap[i].strip()
     return heightMap
 
+def generateNodes(heightMap):
+    for i in range(len(heightMap)): #iterate rows - bottom to top
+        row = heightMap[(i + 1)*-1]
+        Node._registry.append([])
+        for j in range(len(row)): #iterate columns - left to right
+            node = Node(i, j, row[j])
+            if j == 'S':
+                node.start = True
+                node.elev = 'a'
+            if j == 'E':
+                node.end = True
+                node.elev = 'z'
+
+def mapAdjacentNodes():
+    #write a function to relate each node above, below, and to each side
+    pass
+
+class Node:
+    _registry = []
+
+    def __init__(self, row, column, elev, start = False, end = False, above = None, below = None, right = None, left = None):
+        self.row = row
+        self.column = column
+        self.elev = elev
+        self.start = start
+        self.end = end
+        self.above = above
+        self.below = below
+        self.right = right
+        self.left = left
+        Node._registry[row].append(self)
+
 heightMap = readFile()
-print("Heightmap: ", heightMap)
+nodes = generateNodes(heightMap)
